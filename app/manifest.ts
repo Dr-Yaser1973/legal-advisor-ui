8a0a3dbaf68c71f14fa4c642bce6afae5e73a24d
yaser <yaseralzbadi@gmail.com>
2026-05-22 00:24:14 +0300

    fix icons path

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,17 +11,17 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#0b1220",
     lang: "ar",
     dir: "rtl",
-    icons: [
-      {
-        src: "/icons/icon-192x192.png", // ← صحّحنا الاسم
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        src: "/icons/icon-512x512.png", // ← صحّحنا الاسم
-        sizes: "512x512",
-        type: "image/png",
-      },
-    ],
+     icons: [
+  {
+    src: "/icons/icon-192.png",
+    sizes: "192x192",
+    type: "image/png",
+  },
+  {
+    src: "/icons/icon-512.png",
+    sizes: "512x512",
+    type: "image/png",
+  },
+],
   };
 }
